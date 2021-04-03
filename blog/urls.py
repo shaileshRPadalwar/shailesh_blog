8b7eddecc063bbd84908c4885532f03105689a5c
shailesh12345 <shaileshpadalwar12345>
2021-04-03 12:11:58 +0530

    tag

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,8 @@ from blog import views
 
 urlpatterns = [
     path('',views.post_list_view),
+    path('tag/<slug:tag_slug>/',views.post_list_view,name='post_list_by_tag_name'),
     path('post/<slug:slug>/',views.post_detail,name='post_detail'),
     path('blog/',views.PostListView.as_view()),
     path('mail/<id>/',views.mail_send_view),
 ]
-
-
